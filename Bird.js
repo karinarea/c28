1dbdd84d6260dbd03dc99aa0c2ffd2da91486761
Karina <karinarearea@gmail.com>
2020-10-08 16:53:26 -0700

    slingshot effect

--- Bird.js
+++ Bird.js
@@ -1,12 +1,23 @@
-class Bird extends BaseClass {
+class Bird  {
   constructor(x,y){
-    super(x,y,50,50);
-    this.image = loadImage("sprites/bird.png");
+    var options = {
+      'restitution':0.8,
+      'friction':1.0,
+      'density':1.7
   }
-
-  display() {
-    this.body.position.x = mouseX;
-    this.body.position.y = mouseY;
-    super.display();
-  }
+  this.body = Bodies.rectangle(x, y, 50, 50, options);
+  this.width = 50;
+  this.height = 50;
+  this.image = loadImage("sprites/bird.png");
+  World.add(world, this.body);
+}
+display(){
+  var angle = this.body.angle;
+  push();
+  translate(this.body.position.x, this.body.position.y);
+  rotate(angle);
+  imageMode(CENTER);
+  image(this.image, 0, 0, this.width, this.height);
+  pop();
+}
 }
